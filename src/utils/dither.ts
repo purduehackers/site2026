ef10c222272d40df0f6735abe549e4e0cd712b3c
Alicia Zhou <zhou1447@purdue.edu>
2026-02-02 22:14:31 -0500

    fixed join button + image dither

--- src/utils/dither.ts
+++ src/utils/dither.ts
@@ -17,7 +17,7 @@ export function getHeroDitherSvg(width = 4, height = 128): string {
 			}
 		}
 	}
-	const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}"><rect width="100%" height="100%" fill="#fff"/>${rects.join('')}</svg>`;
+	const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}"><rect width="100%" height="100%" fill="#fffbf1"/>${rects.join('')}</svg>`;
 	return 'data:image/svg+xml,' + encodeURIComponent(svg);
 }
 
