0a813cf62437f7de15f6563177c6745c5cdd020f
Matthew Stanciu <mattbstanciu@gmail.com>
2026-02-08 23:41:44 -0800

    Some tweaks (#1)
    * small thing on the hack night section
    
    * switch to woff2
    
    * oops
    
    * target=_blank
    
    * add discord to header
    
    * use astro fonts
    
    * remove merch

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	safelist: ['font-inconsolata'],
-	theme: {
-		extend: {
-			fontFamily: {
-				inconsolata: ['"Inconsolata"', 'monospace'],
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: ["font-inconsolata"],
+  theme: {
+    extend: {
+      fontFamily: {
+        whyte: [
+          "var(--font-whyte)",
+          "system-ui",
+          "-apple-system",
+          "sans-serif",
+        ],
+        inconsolata: ["var(--font-inconsolata)", "monospace"],
+      },
+    },
+  },
+  plugins: [],
 };
