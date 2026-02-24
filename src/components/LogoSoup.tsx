f572e83a95dad4ec82d12903229795787161f0d4
Alicia Zhou <zhou1447@purdue.edu>
2026-02-23 22:49:45 -0500

    logo soup finally and hack night alignment fixes

--- src/components/LogoSoup.tsx
+++ src/components/LogoSoup.tsx
@@ -1,26 +1,25 @@
-interface LogoSoupProps {
+interface LogoGridProps {
   logos: string[];
-  baseSize?: number;
-  scaleFactor?: number;
+  showGrid?: boolean;
   className?: string;
 }
 
-export default function LogoSoup({
-  logos,
-  baseSize = 32,
-  className,
-}: LogoSoupProps) {
+export default function LogoGrid({ logos, showGrid = true, className = '' }: LogoGridProps) {
+  const gridLine = showGrid ? 'logo-grid-line' : '';
+
   return (
-    <div className={className}>
+    <div className={`flex flex-wrap justify-center ${className}`}>
       {logos.map((src) => (
-        <img
+        <div
           key={src}
-          src={src}
-          alt=""
-          height={baseSize}
-          style={{ height: baseSize, width: "auto" }}
-          className="inline-block p-2 align-middle"
-        />
+          className={`logo-grid-cell flex items-center justify-center w-1/2 sm:w-1/3 md:w-1/4 p-6 md:p-8 ${gridLine}`}
+        >
+          <img
+            src={src}
+            alt=""
+            className="logo-grid-img h-8 md:h-10 w-auto max-w-full object-contain"
+          />
+        </div>
       ))}
     </div>
   );
